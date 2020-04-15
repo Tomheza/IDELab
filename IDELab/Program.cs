302297dcae077c06f900a08d6fadef43a8002682
Tomheza <zemgtomas@gmail.com>
2020-04-15 08:25:02 +0300

    Add project files.

---
+++ IDELab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IDELab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integruotu programavimo aplinku laboratorinis darbas!");
+
+
+        }
+    }
+}
