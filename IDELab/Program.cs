6d46230275bf42b4156f8b950cdddfd80bb12ef7
Tomheza <zemgtomas@gmail.com>
2020-04-16 08:32:57 +0300

    [0.1] Implemented 0.1version

--- IDELab/Program.cs
+++ IDELab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IDELab
 {
@@ -6,9 +7,124 @@ namespace IDELab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Integruotu programavimo aplinku laboratorinis darbas!");
+            Console.WriteLine("IDE LAB!");
+
+            var students = new List<Student>();
+
+            while (true)
+            {
+                // Students loop
+                Console.WriteLine("Enter student first name");
+                var firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter student last name");
+                var lastName = Console.ReadLine();
+
+                if (!(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName)))
+                {
+                    var student = new Student() {FirstName = firstName, LastName = lastName};
+
+                    Console.WriteLine("If you wish to get a random set of results enter 0, otherwise any key.");
+                    var homeworkResultTypeInput = Console.ReadLine();
+
+                    if (int.TryParse(homeworkResultTypeInput, out var homeWorkResultType) && homeWorkResultType == 0)
+                        GenerateRandomStudentResults(student);
+
+                    else
+                        SetUserResultsManually(student);
+
+                    students.Add(student);
+                }
+
+                else
+                {
+                    Console.WriteLine("You did not input first or last name of the student. Try again.");
+                }
+
+                Console.WriteLine("If you want to finish entering results and see the output enter: /");
+
+                if (Console.ReadLine() == "/")
+                    break;
+            }
+
+            Console.WriteLine("If you want to see results with average homework result enter 0, if median enter 1");
+            var outputInput = Console.ReadLine();
+
+            bool useMedian;
+            if (int.TryParse(outputInput, out var outputOption) && outputOption == 1)
+                useMedian = true;
+            else
+                useMedian = false;
+
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine($"{"Last Name",-10}\t{"First Name",-10}\t{"Final (Avg.)",10}");
+            Console.WriteLine("-------------------------------------------------------");
+            students.ForEach(x => x.OutputStudentResults(useMedian));
+        }
+
+        private static void SetUserResultsManually(Student student)
+        {
+            var resultList = new List<int>();
+
+            while (true)
+            {
+                // Homework results
+
+                Console.WriteLine("Enter student's homework results. If you wish to stop entering homework results enter: /");
+                var homeworkInput = Console.ReadLine();
+
+                if (int.TryParse(homeworkInput, out var homeworkResult) && homeworkResult < 11 && homeworkResult > 0)
+                {
+                    student.HomeworkResults.Add(homeworkResult);
+                    resultList.Add(homeworkResult);
+                }
+                
+                else if (homeworkInput == "/")
+                {
+                    student.HomeworkResultsInArray = resultList.ToArray();
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Entered value was in an incorrect format, the result will be ignored.");
+                }
+            }
 
 
+            Console.WriteLine("Enter exam result");
+
+            while (true)
+            {
+                var examInput = Console.ReadLine();
+
+                if (int.TryParse(examInput, out var examResult) && examResult < 11 && examResult > 0)
+                {
+                    student.ExamResult = examResult;
+                    break;
+                }
+
+                Console.WriteLine("Entered exam value was incorrect, try again.");
+            }
+        }
+
+        public static void GenerateRandomStudentResults(Student student)
+        {
+            var rnd = new Random();
+
+            var resultCount = rnd.Next(1, 11);
+
+            for (var i = 0; i < resultCount; i++)
+            {
+
+                var randomGeneratedNumber = rnd.Next(1, 11);
+                Console.WriteLine($"Random generated homework result: {randomGeneratedNumber}");
+                student.HomeworkResults.Add(randomGeneratedNumber);
+            }
+
+            var randomExamResult = rnd.Next(1, 11);
+            Console.WriteLine($"Random exam result: {randomExamResult}");
+            student.ExamResult = randomExamResult;
         }
     }
 }
---
+++ IDELab/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IDELab
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<int> HomeworkResults { get; } = new List<int>();
+        public int[] HomeworkResultsInArray { get; set; }
+        public int ExamResult { get; set; }
+        public double AverageHomeworkResult => (double) HomeworkResults.Sum(x => x) / HomeworkResults.Count;
+        public double MedianHomeworkResult => GetMedian();
+        public double MedianHomeworkResultArray => GetMedianFromArray();
+        public double AverageHomeworkResultArray => (double) HomeworkResultsInArray.Sum(x => x) / HomeworkResultsInArray.Length;
+
+        private double GetMedian()
+        {
+            if (!HomeworkResults.Any())
+                return 0;
+
+            var sortedNumbers = new List<int>(HomeworkResults);
+            sortedNumbers.Sort();
+
+            return GetMedianNumber(sortedNumbers);
+        }
+
+        private double GetMedianFromArray()
+        {
+            if (!HomeworkResultsInArray.Any())
+                return 0;
+
+            var clonedArray = new int[HomeworkResultsInArray.Length];
+            Array.Copy(HomeworkResultsInArray, clonedArray, HomeworkResultsInArray.Length);
+            Array.Sort(HomeworkResultsInArray);
+
+            return GetMedianNumber(HomeworkResultsInArray);
+        }
+
+        private static double GetMedianNumber(IReadOnlyList<int> sortedNumbers)
+        {
+            var midNumber = sortedNumbers.Count / 2;
+
+            var median = (sortedNumbers.Count % 2 != 0)
+                ? sortedNumbers[midNumber]
+                : ((double)sortedNumbers[midNumber] + sortedNumbers[midNumber - 1]) / 2;
+
+            return median;
+        }
+
+        public void OutputStudentResults(bool useMedian)
+        {
+            Console.WriteLine($"{LastName,-10}\t{FirstName,-10}\t{GetFinalResult(useMedian),10:N}");
+        }
+
+        public double GetFinalResult(bool useMedian)
+        {
+            return 0.7 * ExamResult + 0.3 * (useMedian ? MedianHomeworkResult : AverageHomeworkResult);
+        }
+    }
+}
